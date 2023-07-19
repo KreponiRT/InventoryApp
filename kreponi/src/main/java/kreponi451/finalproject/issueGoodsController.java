3e536a180df565a2cff6a9805618e56da7545d28
KreponiRT <kreponi67rt@gmail.com>
2023-07-19 17:49:47 +0000

    Initial Commit

---
+++ kreponi/src/main/java/kreponi451/finalproject/issueGoodsController.java
@@ -0,0 +1,4 @@
+package kreponi451.finalproject;
+
+public class issueGoodsController {
+}
---
+++ kreponi/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module kreponi451.userlogin {
+
+    requires  javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires mysql.connector.j;
+    requires fontawesomefx;
+
+
+    opens kreponi451.finalproject;
+
+}s
